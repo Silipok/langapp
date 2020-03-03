81106b20826293b6f24f8f660d399855dd54b391
scarferok <scarferok@gmail.com>
2020-03-03 08:56:41 +0300

    add test vocab service

--- src/app/services/vocabulary.service.spec.ts
+++ src/app/services/vocabulary.service.spec.ts
@@ -3,10 +3,19 @@ import { TestBed } from '@angular/core/testing';
 import { VocabularyService } from './vocabulary.service';
 
 describe('VocabularyService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: VocabularyService = TestBed.get(VocabularyService);
-    expect(service).toBeTruthy();
+  let service: VocabularyService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = new VocabularyService();
+  });
+  it('init vocabular', () => {
+    service.initVocab('en');
+    expect(service.vocabular).toEqual([]);
   });
 });
+
+it('should be created', () => {
+    const service: VocabularyService = TestBed.get(VocabularyService);
+    expect(service).toBeTruthy();
+  }
+);
